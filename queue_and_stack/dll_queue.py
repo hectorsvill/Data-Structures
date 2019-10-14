3dc89ab5804198014d796998630b6719e78e16de
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-14 14:57:35 -0700

    enqueue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,46 @@
 import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import ListNode
 
+
+""" 
+
+back [0][1][2][3] front
+
+"""
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # use dll because it allowes for asy filo
+        self.storage = ListNode(None)
 
+    """Add item to the end of the queue
+    """
     def enqueue(self, value):
-        pass
+        if self.size == 0:
+            # first link
+            self.storage = ListNode(value)
+        else:
+            # add to the front
+            self.storage = self.storage.add_to_head(value)
 
+
+        # link added
+        self.size += 1
+
+        
     def dequeue(self):
-        pass
+
+        # link removed
+        self.size -= 1
 
     def len(self):
-        pass
+        return self.size
+
+
+q = Queue()
+
+# print(q.size)
