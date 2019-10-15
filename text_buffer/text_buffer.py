eada8a0f083422c8a46136816f409b54d7a06d79
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-15 10:23:29 -0700

    text buffer

---
+++ text_buffer/text_buffer.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+class TextBuffer:
+    def __init__(self, init=none):
+        pass
+
+    def appened(self, string_to_add):
+        pass
+    
+    def prepend(self, string_to_add):
+        pass
+
+    def delete_back(self, chars_to_remove):
+        pass
+
+    def join(self, other_buffer):
+        pass
+
+    def split(self, split_location):
+        pass 
+
+    def join_string(self, string_to_join):
+        pass
+
+if __name__ == "__main__":
+    pass
+
